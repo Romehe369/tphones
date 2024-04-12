b5b1d2553d018c72f18f9c5f81d22b8c7cba5304
Ronald Huahuatico Soria <69460625+Romehe369@users.noreply.github.com>
2024-04-11 21:03:45 -0500

    Login c

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,9 @@ import { Routes } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { AddEditComponent } from './components/dashboard/add-edit/add-edit.component';
 import { ListUserComponent } from './components/dashboard/list-user/list-user.component';
+import { LoginComponent } from './components/login/login.component';
 
-export const routes: Routes = [{ path: "", component: ListUserComponent },
+export const routes: Routes = [{ path: "", component: LoginComponent },
 { path: "home", component: DashboardComponent},
 { path: "edit/:id", component: AddEditComponent },
 { path: 'list', component: ListUserComponent}];
